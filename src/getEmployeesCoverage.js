6ca07904b850268946e985a354d228fc3ee7e88e
Dear_Jao <jvvr2013@gmail.com>
2022-06-02 21:15:26 -0300

    requisito 11 completado

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,38 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const allSpecies = (animalsId) => animalsId.map((element) =>
+  data.species.find((animalData) => animalData.id === element).name);
+
+const locations = (specie) => specie.map((element) =>
+  data.species.find((cordenates) => cordenates.name === element).location);
+
+const finalObj = (employee) => {
+  const { id, firstName, lastName, responsibleFor } = employee;
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: allSpecies(responsibleFor),
+    locations: locations(allSpecies(responsibleFor)),
+  };
+};
+
+const allEmployees = () => data.employees.map((obj) => finalObj(obj));
+
+function getEmployeesCoverage(param) {
+  if (param === undefined) {
+    return allEmployees();
+  }
+
+  const { name, id } = param;
+  const employeesObj = data.employees;
+
+  const employee = () => employeesObj.find((names) => names.firstName === name
+  || names.lastName === name || names.id === id);
+
+  if (employee() === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return finalObj(employee());
 }
 
 module.exports = getEmployeesCoverage;
